7821f7f3661e5c66865d3129a42e5e81bf6e34dc
jahangir <jahangiralomtuf99@gmail.com>
2024-07-08 01:04:22 +0600

    added cart data deleted

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import useAxios from "../../Hooks/useAxios";
 import useAuthProvider from "../../Hooks/useAuthProvider";
+import { FaArrowRightLong } from "react-icons/fa6";
+import Swal from "sweetalert2";
 
 const Cart = () => {
   const axios = useAxios();
   const { user } = useAuthProvider();
   const [data, setData] = useState([]);
-  const [number, setNumber] = useState(1);
 
   useEffect(() => {
     fetchItems();
@@ -17,151 +18,138 @@ const Cart = () => {
     setData(response.data);
   };
 
+  const totalPrice = data?.reduce((pre, sum) => pre + sum.price, 0);
+  const mainPrice = totalPrice.toFixed(2);
   // console.log(data);
 
-  const handleMinusBtn = () => {
-    if (number > 1) {
-      setNumber(number - 1);
-    }
+  // handleDelete
+  const handleDelete = async (id) => { 
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axios.delete(`/delate-cart/${id}`);
+        if (res.data.deletedCount > 0) {
+          fetchItems();
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success",
+          });
+        }
+      }
+    });
   };
 
-  const handlePlusBtn = () => {
-    setNumber(number + 1);
-  };
 
   return (
-    <div className="mt-8 max-w-screen-xl mx-auto px-4 bg-[#f9f9f9]">
-      <div className="flex flex-col lg:flex-row gap-3 mt-12">
-        <div className="lg:w-[70%] ">
-          <div className=" overflow-x-auto">
-            <table className=" w-full min-w-max table-auto text-left ">
-              <thead>
-                <tr>
-                  <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p className="text-sm text-blue-gray-900 font-bold leading-none opacity-70">
-                      No
-                    </p>
-                  </th>
-                  <th className="cursor-pointer border-y  border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p className="text-sm text-blue-gray-900  leading-none opacity-70 font-bold">
-                      PRODUCT
-                    </p>
-                  </th>
-                  <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p className="text-sm text-blue-gray-900  font-bold leading-none opacity-70">
-                      PRICE
-                    </p>
-                  </th>
-                  <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p className="text-sm text-blue-gray-900  font-bold leading-none opacity-70">
-                      QUANTITY
-                    </p>
-                  </th>
-                  <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
-                    <p className="text-sm text-blue-gray-900  font-bold leading-none opacity-70">
-                      SUBTOTAL
-                    </p>
-                  </th>
+    <div className="max-w-screen-xl mx-auto mt-6 px-4">
+      <h1 className="text-center text-3xl font-bold ">All cart Products :</h1>
+      <div
+        className={`h-96 overflow-y-scroll mt-5`}
+      >
+        <table className="mt-4 w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="text-sm text-blue-gray-900 font-bold leading-none opacity-70">
+                  #
+                </p>
+              </th>
+              <th className="cursor-pointer border-y  border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="text-sm text-blue-gray-900  leading-none opacity-70 font-bold">
+                  Product Image
+                </p>
+              </th>
+              <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="text-sm text-blue-gray-900  font-bold leading-none opacity-70">
+                  Product Name
+                </p>
+              </th>
+              <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="text-sm text-blue-gray-900  font-bold leading-none opacity-70">
+                  Product Price
+                </p>
+              </th>
+              <th className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50">
+                <p className="text-sm text-blue-gray-900  font-bold leading-none opacity-70">
+                  Action
+                </p>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((table, idx) => {
+              return (
+                <tr key={table._id}>
+                  <td className="p-4 border-b border-blue-gray-50">
+                    <div>
+                      <p>{idx + 1}</p>
+                    </div>
+                  </td>
+                  <td className="p-4 border-b border-blue-gray-50">
+                    <div>
+                      <img
+                        className="size-32 border rounded-lg"
+                        src={table.image}
+                        alt=""
+                      />
+                    </div>
+                  </td>
+                  <td className="p-4 border-b border-blue-gray-50">
+                    <p>{table.name}</p>
+                  </td>
+                  <td className="p-4 border-b border-blue-gray-50">
+                    <p className="font-bold">{table.price} $</p>
+                  </td>
+                  <td
+                    onClick={() => handleDelete(table._id)}
+                    className="p-4 border-b border-blue-gray-50"
+                  >
+                    <button className="flex w-12 items-center text-white justify-center bg-red-500 p-1 rounded-lg cursor-pointer hover:bg-red-700">
+                      X
+                    </button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {data.map((item) => (
-                  <tr key={item._id}>
-                    <td className="p-4 border-b border-blue-gray-50">
-                      <div>
-                        <p>1</p>
-                      </div>
-                    </td>
-                    <td className="p-4 border-b border-blue-gray-50">
-                      <div className="flex items-center gap-3">
-                        <img
-                          className="size-16 border rounded-lg"
-                          src={item.image}
-                          alt=""
-                        />
-                        <p className="font-semibold ">{item.name}</p>
-                      </div>
-                    </td>
-
-                    <td className="p-4 border-b border-blue-gray-50">
-                      {" "}
-                      <p>{item.price} $</p>
-                    </td>
-                    <td className="p-4 border-b border-blue-gray-50">
-                      <div className="font-bold flex items-center cursor-pointer">
-                        <p
-                          onClick={handleMinusBtn}
-                          className="border text-xl p-1 rounded-s-full px-3"
-                        >
-                          -
-                        </p>
-
-                        <p className="border text-xl p-1  px-3">{number}</p>
-                        <p
-                          onClick={handlePlusBtn}
-                          className="border text-xl py-1 rounded-r-full px-3"
-                        >
-                          +
-                        </p>
-                      </div>
-                    </td>
-                    <td className="p-4 border-b border-blue-gray-50">
-                      <p className="text-xl text-[#db3a87]">350৳ </p>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <hr className="mt-8" />
-          <div className="mt-8 flex flex-col-reverse gap-6 lg:flex-row justify-between items-center">
-            <div className="flex gap-4 items-center">
-              <input
-                className="border rounded-full py-2 px-4"
-                type="text"
-                name=""
-                id=""
-                placeholder="Coupon Code"
-              />
-              <button className="py-2 px-6 bg-red-500 hover:bg-red-800 font-bold text-white rounded-full text-sm">
-                APPLY COUPON
-              </button>
-            </div>
-            <div>
-              <button className="py-2 px-6 bg-red-400 hover:bg-red-800 font-bold text-white rounded-full text-sm">
-                UPDATE CART
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="lg:w-[30%] bg-white border-2 rounded p-5">
-          <h1 className="text-2xl font-semibold">CART TOTALS</h1>
-          <div className="flex items-center justify-between mt-6">
-            <p className="font-semibold text-xl">Subtotal </p>
-            <p className="font-bold">350৳</p>
-          </div>
-          <div className="mt-6 flex items-center justify-between">
-            <p className="font-semibold text-xl">Shipping</p>
-            <div className="flex flex-col gap-3">
-              <p>Delivery Charge (Free)</p>
-              <p className="text-[#77777]">
-                Shipping to <span className="font-bold">Kishoreganj.</span>
-              </p>
-              <p className="text-[#db3a87] text-xl font-semibold">
-                Change address
-              </p>
-            </div>
-          </div>
-          <hr className="mt-4" />
-          <div className="mt-5 flex items-center justify-between">
-            <p className="font-semibold text-xl">Total</p>
-            <p className="text-xl font-semibold text-[#db3a87]">350৳ </p>
-          </div>
-          <div className="bg-red-500 hover:bg-red-800 rounded-full text-center p-2 mt-6">
-            <button className="text-white text-sm font-bold">
-              PROCEED TO CHECKOUT
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="mt-12 md:flex justify-between items-center text-center ">
+        <div className="md:flex items-center">
+          <input
+            type="text"
+            className="border border-black p-3 text-red-500 rounded-md"
+            placeholder="Coupon Code"
+          />
+          <div className="mt-12 md:mt-0">
+            <button
+              // onClick={handleApply}
+              className="border p-3 ml-2 rounded-md px-12 bg-[#ef6f18] text-white font-bold hover:bg-[#a35b2b]"
+            >
+              Apply
             </button>
           </div>
+        </div>
+        <div>
+          <h1 className="text-xl font-bold uppercase mt-12 md:mt-0">
+            Total Price : <span className="text-[#ef6f18]">{mainPrice} $</span>
+          </h1>
+        </div>
+        <div className="flex items-center justify-center mt-12 md:mt-0">
+          <button
+            // onClick={handleApply}
+            className="border  p-3 ml-2 rounded-md px-12 bg-[#ef6f18] text-white font-bold hover:bg-[#a35b2b] flex items-center gap-4"
+          >
+            Purchase All <FaArrowRightLong />
+          </button>
         </div>
       </div>
     </div>
