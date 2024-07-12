9658bd07aceb67e7de16ab175b465dc68e47f161
jahangir <jahangiralomtuf99@gmail.com>
2024-07-12 16:42:33 +0600

    added api

--- src/Hooks/useAxios.jsx
+++ src/Hooks/useAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://carid-project-server-production.up.railway.app/api/v1",
 });
 const useAxios = () => {
   return instance;
