78675aa13b578c20ca5345b3b5d0eab040d47104
jahangir <jahangiralomtuf99@gmail.com>
2024-07-17 23:21:31 +0600

    added api link changes

--- src/Components/SubCategory/SubCategory.jsx
+++ src/Components/SubCategory/SubCategory.jsx
@@ -14,7 +14,9 @@ const SubCategory = () => {
 
   useEffect(() => {
     axios
-      .get(`/sub-category?subCategory=${category}&&name=${search}&&order=${highTOLow}`)
+      .get(
+        `/sub-category?subCategory=${category}&&name=${search}&&order=${highTOLow}`
+      )
       .then((res) => {
         setSubCat(res?.data);
         // console.log(res);
@@ -40,21 +42,47 @@ const SubCategory = () => {
       <section className="max-w-screen-xl mx-auto px-3 mt-10">
         <div>
           <div className="flex items-center justify-between">
-            <form onChange={(e) => setSearch(e.target.value)} className="w-[30vw] ">
-              <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <form
+              onChange={(e) => setSearch(e.target.value)}
+              className="w-[30vw] "
+            >
+              <label
+                htmlFor="default-search"
+                className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+              >
+                Search
+              </label>
               <div className="relative">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                  <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                  <svg
+                    className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 20"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                    />
                   </svg>
                 </div>
-                <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-
+                <input
+                  type="search"
+                  id="default-search"
+                  className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search Mockups, Logos..."
+                  required
+                />
               </div>
             </form>
             <div>
               <div className="relative my-6 ">
-                <select onChange={(e) => setHighTOLow(e.target.value)}
+                <select
+                  onChange={(e) => setHighTOLow(e.target.value)}
                   id="id-04"
                   name="id-04"
                   required
